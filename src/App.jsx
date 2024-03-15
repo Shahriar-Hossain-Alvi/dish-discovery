b27b47aab7106745f140cb1589551a1641d3aeab
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-03-16 00:26:48 +0600

    created project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xl font-semibold bg-red-400 underline">
+        Hello world!
+      </h1>
+      <button className='btn btn-primary'>click</button>
+    </>
+  )
+}
+
+export default App
