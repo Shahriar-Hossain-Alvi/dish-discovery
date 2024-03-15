f612c765631071e7a521366f3f9d94eb33eb57d2
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-03-16 02:05:44 +0600

    added recipe head

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,8 +5,8 @@ const Banner = () => {
             <div className="hero rounded-xl banner-img">
                 <div className="hero-content text-center text-neutral-content">
                     <div className='py-32 px-20 space-y-5'>
-                        <h1 className="text-white lexend text-5xl font-bold">Discover an exceptional cooking class tailored for you!</h1>
-                        <p className="text-white lexend text-lg">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.</p>
+                        <h1 className="text-white lexend text-5xl font-bold">Savor Every Bite: Unveiling a Feast of Flavor and Creativity</h1>
+                        <p className="text-white lexend text-lg">Indulge in a culinary journey with our exquisite recipes, meticulously crafted to tantalize your taste buds. Discover a symphony of flavors and aromas, right at your fingertips.</p>
                         <div className='space-x-5'>
                             <button className='btn btn-lg rounded-full bg-[#0BE58A] text-[#150B2B] lexend text-xl font-semibold border border-[#0BE58A] hover:bg-transparent hover:border-white hover:text-white'>Explore Now</button>
 
---
+++ src/components/RecipeHead/RecipeHead.jsx
@@ -0,0 +1,12 @@
+const RecipeHead = () => {
+    return (
+        <div className="container mx-auto mb-10">
+            <div className="text-center">
+                <h2 className="text-4xl font-semibold text-[#150B2B] lexend mb-3">Our Recipes</h2>
+                <p className="lexend text-[#150B2B99]">Explore a World of Culinary Delights Created with Passion and Precision</p>
+            </div>
+        </div>
+    );
+};
+
+export default RecipeHead;
